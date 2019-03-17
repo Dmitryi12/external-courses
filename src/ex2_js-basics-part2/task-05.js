db2bc3b9c917b2de5d405c2052b91aaa7b35a0b5
Dmitryi12 <dmitryi1206@gmail.com>
2019-03-17 23:35:35 +0300

    created function accepts number or string value depending on the type of argument; created function showing array elements total amount elements; created function which counts the number of even and odd elements in array; created function checking same elements in array; created function finding max value elements in array

--- src/ex2_js-basics-part2/task-05.js
+++ src/ex2_js-basics-part2/task-05.js
@@ -1,7 +1,4 @@
-function maxarr (arr) {
-       var max = arr[0]
-        for (var i = 0; i < arr.length; i++) {
-                if (arr[i] > max) {max = arr[i]}  
-                else {max = arr[0]}
-}return max}
+function maxarr(arr) {
+  return Math.max.apply(undefined, arr);
+}
 module.exports=maxarr
