a22e94fcc387dfd31ecad380bccd415036a7832d
Dmitryi12 <dmitryi1206@gmail.com>
2019-03-20 15:07:57 +0300

    In task-01.js renamed name of functiion. Replaced isNaN(arg)===false. Removed last conditions else and unnecessary round brackets; In task-02.js removed unnecessary round brackets and renamed name of function

--- src/ex2_js-basics-part2/task-02.js
+++ src/ex2_js-basics-part2/task-02.js
@@ -1,8 +1,8 @@
-var funcarr = function (arr) {
+var funccarr = function (arr) {
 	for (var i = 0; i < arr.length; i++) {
 		console.log (arr[i]);
 		} 
 		console.log (arr.length)
 	return
 }
-module.exports=funcarr
+module.exports=funccarr
